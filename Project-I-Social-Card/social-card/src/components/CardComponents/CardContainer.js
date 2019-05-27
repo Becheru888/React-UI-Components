1e85f3886f16aa76b2e7d948347635119599a88f
Becheru888 <becheru93@gmail.com>
2019-05-27 22:39:11 +0100

    Header added

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Card.css';
+import './CardBanner';
+
+
+function CardContainer(){
+    return (
+        <div>
+           
+        </div>
+    )
+}
+
+export default CardContainer
